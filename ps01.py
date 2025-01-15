509126b5cb7033e5dbe6a7f72ead4a2c13581f94
MrYori111 <202480030@psu.palawan.edu.ph>
2025-01-15 12:13:36 +0800

    Add files via upload

---
+++ ps01.py
@@ -0,0 +1,18 @@
+# Python Program with Intentional Errors
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+
+current_year = 2025
+years_left = 100 - age
+
+print("Hello " + name + ",")
+print("You will turn 100 years old in the year " + current_year + years_left)
+
+
+2nd_number = int(input("Enter a second number: "))
+
+
+sum = first_number int("2nd_number")
