32c193186ac230e99aeef06a98d27d3f00f5854d
MrYori111 <202480030@psu.palawan.edu.ph>
2025-01-15 12:18:42 +0800

    Add files via upload

---
+++ ps02.py
@@ -0,0 +1,1 @@
+# Create a Python program that computes the area of a triangle using Heron's Formula. Instead of using the sqrt() function from the math module, use exponentiation to calculate the square root.
