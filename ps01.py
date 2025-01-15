cfc54500cbe078cd15bc0a341a1caba52d59b72e
Dominic44th <202480080@psu.palawan.edu.ph>
2025-01-15 13:04:44 +0800

    Update ps01.py

--- ps01.py
+++ ps01.py
@@ -2,17 +2,17 @@
 
 
 name = input("Enter your name: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 
 
 current_year = 2025
 years_left = 100 - age
 
 print("Hello " + name + ",")
-print("You will turn 100 years old in the year " + current_year + years_left)
+print("You will turn 100 years old in the year " , current_year + years_left)
 
+First_number = int(input("Enter a First number: "))
+Second_number = int(input("Enter a Second number: "))
 
-2nd_number = int(input("Enter a second number: "))
-
-
-sum = first_number int("2nd_number")
+sum = First_number + Second_number
+print(sum)
